ef4f04de52e11e174144790d8994050e351c5bd2
Tuan Ngo <ngotuan911@gmail.com>
2022-01-11 01:56:57 -0800

    Fixed animation of button

--- src/component/searchbox/searchbox.component.jsx
+++ src/component/searchbox/searchbox.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import "./searchbox.styles.scss";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 const SearchBox = () => {
   const [isActive, setActive] = useState(false);
 
   const toggleClass = () => {
 
     setActive(!isActive);
+  };
+  const input = useRef(null);
+  const handleFocus = () => {
+    input.current.focus();
   }
   return (
-    <div className="searchbox">
-      <input autoFocus className={ isActive ? 'searchbox__input open' : 'searchbox__input'} ></input>
-      <button className='searchbox__btn' onClick={toggleClass}
+    <div className={`searchbox`}>
+      <input ref={input} className={`searchbox__input${isActive ? ' open' : ''}`} ></input>
+
+      <button className={`searchbox__btn${isActive ? ' open' : ''}`} onClick={() => {
+        toggleClass();
+        handleFocus();
+      }}
 
       ><i className="fas fa-search"></i></button>
     </div>
