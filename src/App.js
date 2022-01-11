46024154079729821d654da7b4144075b20fcff2
Tuan Ngo <ngotuan911@gmail.com>
2022-01-10 23:58:44 -0800

    Created Github-page for react-searchbox

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import SearchBox from './component/searchbox/searchbox.component';
 function App() {
   return (
-    <div className="App">
+
       <SearchBox></SearchBox>
-    </div>
+
   );
 }
 
--- src/component/searchbox/searchbox.component.jsx
+++ src/component/searchbox/searchbox.component.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./searchbox.styles.scss";
-
+import { useState } from 'react';
 const SearchBox = () => {
-  return (
-    <div>
+  const [isActive, setActive] = useState(false);
 
+  const toggleClass = () => {
+
+    setActive(!isActive);
+  }
+  return (
+    <div className="searchbox">
+      <input autoFocus className={ isActive ? 'searchbox__input open' : 'searchbox__input'} ></input>
+      <button className='searchbox__btn' onClick={toggleClass}
+
+      ><i className="fas fa-search"></i></button>
     </div>
   );
 };
